584b1f2bd4a4046455e35886c65ec83dc536cc9d
Noah Cohn <noahc42@gmail.com>
2025-07-16 21:33:18 -0700

    Update main.cpp

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
+#include "Game.hpp"
 #include <emscripten/bind.h>
 
-// This is our first C++ function that we will call from JavaScript.
-// It's a simple function that returns an integer.
-int get_player_start_x() {
-    // In the future, this could read from a level file or game state.
-    // For now, we'll just return a hardcoded value.
-    return 100;
-}
-
-// This is the binding code that exposes our C++ function to JavaScript.
-// We are creating a module named "WASM_Venture" and adding our function to it.
+// Using emscripten::bind to expose the C++ classes and functions to JavaScript.
 EMSCRIPTEN_BINDINGS(WASM_Venture) {
-    emscripten::function("getPlayerStartX", &get_player_start_x);
+    // Expose the Position struct and its fields.
+    emscripten::value_object<Position>("Position")
+        .field("x", &Position::x)
+        .field("y", &Position::y);
+
+    // Expose the Game class, its constructor, and its methods.
+    emscripten::class_<Game>("Game")
+        .constructor<>()
+        .function("update", &Game::update)
+        .function("getPlayerPosition", &Game::getPlayerPosition);
 }
