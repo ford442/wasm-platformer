b84ec9401320f737e98475034fae36960fdfdfb2
Noah Cohn <noahc42@gmail.com>
2025-07-16 22:26:27 -0700

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+// This declares a module for our Emscripten glue code.
+// It tells TypeScript that whenever we import '/game.js',
+// it should expect a default export that is a function returning a Promise.
+declare module '/game.js' {
+  const createGameModule: () => Promise<any>;
+  export default createGameModule;
+}
