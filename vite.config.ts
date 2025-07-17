293170479d4a63258a3f1be6dececda0fc42f971
Noah Cohn <noahc42@gmail.com>
2025-07-17 00:23:49 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import wasm from 'vite-plugin-wasm'
-import topLevelAwait from 'vite-plugin-top-level-await'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    // This plugin handles importing .wasm files and their JS glue code.
-    wasm(),
-    // This plugin is required by the wasm plugin to work correctly.
-    topLevelAwait()
-  ],
+  plugins: [react()],
 })
