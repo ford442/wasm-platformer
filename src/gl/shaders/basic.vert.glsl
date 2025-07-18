f0307df49a84c3682a9239258162e72de220e03b
Noah Cohn <noahc42@gmail.com>
2025-07-18 00:51:03 -0700

    Update basic.vert.glsl

--- src/gl/shaders/basic.vert.glsl
+++ src/gl/shaders/basic.vert.glsl
@@ -1,15 +1,19 @@
 #version 300 es
 
-// INPUT: The position of a vertex in the object's local space (a 1x1 square).
 in vec2 a_position;
 
-// UNIFORMS: Data passed from our code.
-uniform vec2 u_model_position; // The center position of the object
-uniform vec2 u_model_size;     // The width and height of the object
+uniform vec2 u_model_position;
+uniform vec2 u_model_size;
+// NEW: A uniform for the camera's world position
+uniform vec2 u_camera_position;
 
 void main() {
-  // Scale the vertex position by the size, then add the world position.
-  vec2 final_position = (a_position * u_model_size) + u_model_position;
+  // Calculate the object's world position
+  vec2 world_position = (a_position * u_model_size) + u_model_position;
+  
+  // To create the scrolling effect, subtract the camera's position from the world position.
+  // This moves the entire world in the opposite direction of the camera.
+  vec2 view_position = world_position - u_camera_position;
 
-  gl_Position = vec4(final_position, 0.0, 1.0);
+  gl_Position = vec4(view_position, 0.0, 1.0);
 }
