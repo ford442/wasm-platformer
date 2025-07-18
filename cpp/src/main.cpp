bbd0f2f13a3ef27c310440a79cc9fe8e00bb9a88
Noah Cohn <noahc42@gmail.com>
2025-07-18 00:49:47 -0700

    Update main.cpp

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -2,24 +2,11 @@
 #include <emscripten/bind.h>
 
 EMSCRIPTEN_BINDINGS(WASM_Venture) {
-    // Expose Vec2 struct
-    emscripten::value_object<Vec2>("Vec2")
-        .field("x", &Vec2::x)
-        .field("y", &Vec2::y);
-        
-    // Expose the Platform struct
-    emscripten::value_object<Platform>("Platform")
-        .field("position", &Platform::position)
-        .field("size", &Platform::size);
-
-    // Expose std::vector<Platform> so we can pass it to JS
+    // ... (Vec2, Platform, PlatformList, InputState bindings remain the same)
+    emscripten::value_object<Vec2>("Vec2").field("x", &Vec2::x).field("y", &Vec2::y);
+    emscripten::value_object<Platform>("Platform").field("position", &Platform::position).field("size", &Platform::size);
     emscripten::register_vector<Platform>("PlatformList");
-
-    // Expose the InputState struct
-    emscripten::value_object<InputState>("InputState")
-        .field("left", &InputState::left)
-        .field("right", &InputState::right)
-        .field("jump", &InputState::jump);
+    emscripten::value_object<InputState>("InputState").field("left", &InputState::left).field("right", &InputState::right).field("jump", &InputState::jump);
 
     // Expose the Game class and its methods
     emscripten::class_<Game>("Game")
@@ -27,5 +14,7 @@ EMSCRIPTEN_BINDINGS(WASM_Venture) {
         .function("update", &Game::update)
         .function("handleInput", &Game::handleInput)
         .function("getPlayerPosition", &Game::getPlayerPosition)
-        .function("getPlatforms", &Game::getPlatforms); // New binding
+        .function("getPlatforms", &Game::getPlatforms)
+        // NEW: Expose the getCameraPosition method to JavaScript
+        .function("getCameraPosition", &Game::getCameraPosition);
 }
