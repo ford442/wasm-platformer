356eafa2d2bf590fcdd7ed33deb61c7e2284d76b
Noah Cohn <noahc42@gmail.com>
2025-07-17 23:02:48 -0700

    Update basic.frag.glsl

--- src/gl/shaders/basic.frag.glsl
+++ src/gl/shaders/basic.frag.glsl
@@ -1,15 +1,13 @@
 #version 300 es
-
-// We must specify the precision for floating point numbers in a fragment shader.
 precision highp float;
 
-// This is the output variable for our shader. It determines the final color of a pixel.
-// It's a 4D vector representing (red, green, blue, alpha).
+// UNIFORM: The color passed from our code.
+uniform vec4 u_color;
+
+// OUTPUT: The final color of the pixel.
 out vec4 outColor;
 
-// This is the main function that runs for every pixel.
 void main() {
-  // We're setting the color to a solid blue with full opacity.
-  // RGBA values are from 0.0 to 1.0.
-  outColor = vec4(0.0, 0.67, 1.0, 1.0); // Our theme's primary color
+  // Set the output color to the value of the uniform.
+  outColor = u_color;
 }
