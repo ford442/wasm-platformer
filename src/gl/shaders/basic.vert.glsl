8d70dca24e46dc030b6c33bbc8e5126bfee0a561
Noah Cohn <noahc42@gmail.com>
2025-07-17 23:02:31 -0700

    Update basic.vert.glsl

--- src/gl/shaders/basic.vert.glsl
+++ src/gl/shaders/basic.vert.glsl
@@ -1,16 +1,15 @@
 #version 300 es
 
-// INPUT: The position of a vertex in the object's local space.
+// INPUT: The position of a vertex in the object's local space (a 1x1 square).
 in vec2 a_position;
 
-// UNIFORM: A global variable passed from our JS/C++ code.
-// This will represent the object's position in the world.
-uniform vec2 u_model_position;
+// UNIFORMS: Data passed from our code.
+uniform vec2 u_model_position; // The center position of the object
+uniform vec2 u_model_size;     // The width and height of the object
 
 void main() {
-  // Calculate the final position by adding the object's world position
-  // to the vertex's local position.
-  vec2 final_position = a_position + u_model_position;
+  // Scale the vertex position by the size, then add the world position.
+  vec2 final_position = (a_position * u_model_size) + u_model_position;
 
   gl_Position = vec4(final_position, 0.0, 1.0);
 }
