c3310cdcc73d60b4b4e4414c1fc296b8118adbd3
Noah Cohn <noahc42@gmail.com>
2025-07-17 22:58:42 -0700

    Update main.cpp

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -6,17 +6,26 @@ EMSCRIPTEN_BINDINGS(WASM_Venture) {
     emscripten::value_object<Vec2>("Vec2")
         .field("x", &Vec2::x)
         .field("y", &Vec2::y);
+        
+    // Expose the Platform struct
+    emscripten::value_object<Platform>("Platform")
+        .field("position", &Platform::position)
+        .field("size", &Platform::size);
 
-    // Expose the new InputState struct so we can create it in JS
+    // Expose std::vector<Platform> so we can pass it to JS
+    emscripten::register_vector<Platform>("PlatformList");
+
+    // Expose the InputState struct
     emscripten::value_object<InputState>("InputState")
         .field("left", &InputState::left)
         .field("right", &InputState::right)
         .field("jump", &InputState::jump);
 
-    // Expose the Game class and its methods, including the new handleInput
+    // Expose the Game class and its methods
     emscripten::class_<Game>("Game")
         .constructor<>()
         .function("update", &Game::update)
-        .function("handleInput", &Game::handleInput) // New binding
-        .function("getPlayerPosition", &Game::getPlayerPosition);
+        .function("handleInput", &Game::handleInput)
+        .function("getPlayerPosition", &Game::getPlayerPosition)
+        .function("getPlatforms", &Game::getPlatforms); // New binding
 }
