c1be1b36823854083bdd4917005257fb80b9b845
Noah Cohn <noahc42@gmail.com>
2025-07-18 05:54:06 -0700

    Update basic.vert.glsl

--- src/gl/shaders/basic.vert.glsl
+++ src/gl/shaders/basic.vert.glsl
@@ -1,14 +1,9 @@
 #version 300 es
 in vec2 a_position;
-in vec2 a_texCoord;
-uniform vec2 u_model_position;
-uniform vec2 u_model_size;
-uniform vec2 u_camera_position;
-out vec2 v_texCoord;
-
+uniform vec2 u_model_position; // The center position of the object
+uniform vec2 u_model_size;     // The width and height of the object
 void main() {
-  vec2 world_position = (a_position * u_model_size) + u_model_position;
-  vec2 view_position = world_position - u_camera_position;
-  gl_Position = vec4(view_position, 0.0, 1.0);
-  v_texCoord = a_texCoord;
+  // Scale the vertex position by the size, then add the world position.
+  vec2 final_position = (a_position * u_model_size) + u_model_position;
+  gl_Position = vec4(final_position, 0.0, 1.0);
 }
