7dad85e83342781e78284a27478c19372b1b5429
Noah Cohn <noahc42@gmail.com>
2025-07-18 05:55:38 -0700

    Update basic.frag.glsl

--- src/gl/shaders/basic.frag.glsl
+++ src/gl/shaders/basic.frag.glsl
@@ -1,9 +1,8 @@
 #version 300 es
 precision highp float;
-in vec2 v_texCoord;
-uniform sampler2D u_texture;
+uniform vec4 u_color;
 out vec4 outColor;
 
 void main() {
-  outColor = texture(u_texture, v_texCoord);
+  outColor = u_color;
 }
