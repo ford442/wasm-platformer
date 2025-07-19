c334e8e3cb27013e19adb3eaad7d47c950df99e7
Noah Cohn <noahc42@gmail.com>
2025-07-19 04:21:56 -0700

    Update main.cpp

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -2,19 +2,26 @@
 #include <emscripten/bind.h>
 
 EMSCRIPTEN_BINDINGS(WASM_Venture) {
-    // ... (Vec2, Platform, PlatformList, InputState bindings remain the same)
+    // ... (Vec2, Platform, etc. bindings are the same)
     emscripten::value_object<Vec2>("Vec2").field("x", &Vec2::x).field("y", &Vec2::y);
     emscripten::value_object<Platform>("Platform").field("position", &Platform::position).field("size", &Platform::size);
     emscripten::register_vector<Platform>("PlatformList");
     emscripten::value_object<InputState>("InputState").field("left", &InputState::left).field("right", &InputState::right).field("jump", &InputState::jump);
 
-    // Expose the Game class and its methods
+    // NEW: Expose the AnimationState struct to JavaScript.
+    emscripten::value_object<AnimationState>("AnimationState")
+        .field("currentState", &AnimationState::currentState)
+        .field("currentFrame", &AnimationState::currentFrame)
+        .field("facingLeft", &AnimationState::facingLeft);
+
+    // Expose the Game class and its methods.
     emscripten::class_<Game>("Game")
         .constructor<>()
         .function("update", &Game::update)
         .function("handleInput", &Game::handleInput)
         .function("getPlayerPosition", &Game::getPlayerPosition)
+        .function("getCameraPosition", &Game::getCameraPosition)
         .function("getPlatforms", &Game::getPlatforms)
-        // NEW: Expose the getCameraPosition method to JavaScript
-        .function("getCameraPosition", &Game::getCameraPosition);
+        // NEW: Expose the animation state getter.
+        .function("getPlayerAnimationState", &Game::getPlayerAnimationState);
 }
