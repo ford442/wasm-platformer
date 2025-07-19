c4f8807280b27d1d9aef5f233ad559bd985b470f
Noah Cohn <noahc42@gmail.com>
2025-07-19 04:28:10 -0700

    Update tex.vert.glsl

--- src/gl/shaders/tex.vert.glsl
+++ src/gl/shaders/tex.vert.glsl
@@ -1,14 +1,21 @@
 #version 300 es
 in vec2 a_position;
 in vec2 a_texCoord;
+
 uniform vec2 u_model_position;
 uniform vec2 u_model_size;
 uniform vec2 u_camera_position;
+
+uniform vec2 u_sprite_frame_size; // The size of one frame in the sheet (e.g., 64x64)
+uniform vec2 u_sprite_sheet_size; // The total size of the spritesheet image
+uniform vec2 u_sprite_frame_coord; // The top-left corner of the current frame (e.g., frame 2, row 1)
+
 out vec2 v_texCoord;
 
 void main() {
   vec2 world_position = (a_position * u_model_size) + u_model_position;
   vec2 view_position = world_position - u_camera_position;
   gl_Position = vec4(view_position, 0.0, 1.0);
-  v_texCoord = a_texCoord;
+  vec2 texelSize = u_sprite_frame_size / u_sprite_sheet_size;
+  v_texCoord = (u_sprite_frame_coord / u_sprite_sheet_size) + (a_texCoord * texelSize);
 }
