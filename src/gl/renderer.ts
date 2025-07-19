e00c2d5a277d0d8c5b7416cf8d3dc89cec3aec97
Noah Cohn <noahc42@gmail.com>
2025-07-19 07:41:03 -0700

    Update renderer.ts

--- src/gl/renderer.ts
+++ src/gl/renderer.ts
@@ -89,8 +89,8 @@ export class Renderer {
         // This prevents bleeding and distortion.
         this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
         this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR_MIPMAP_LINEAR);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR );
 
         this.textureDimensions.set(texture, { width: image.width, height: image.height });
         resolve(texture);
