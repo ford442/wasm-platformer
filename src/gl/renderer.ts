d883d108f76d78a93d7bc8e18114f0f25e4020ce
Noah Cohn <noahc42@gmail.com>
2025-07-19 02:48:31 -0700

    Update renderer.ts

--- src/gl/renderer.ts
+++ src/gl/renderer.ts
@@ -98,15 +98,8 @@ export class Renderer {
       image.onerror = (err) => reject(`Failed to load texture from ${url}: ${err}`);
     });
   }
-
-  private setupUnitSquare() {
-    const positions = new Float32Array([-0.5, -0.5, 0.5, -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5]);
-    this.unitSquarePositionBuffer = this.gl.createBuffer();
-    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.unitSquarePositionBuffer);
-    this.gl.bufferData(this.gl.ARRAY_BUFFER, positions, this.gl.STATIC_DRAW);
-  }
   
-  private setupUnitSquare2() {
+  private setupUnitSquare() {
     const positions = new Float32Array([-0.5, -0.5, 0.5, -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5]);
     this.unitSquarePositionBuffer = this.gl.createBuffer();
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.unitSquarePositionBuffer);
@@ -146,16 +139,16 @@ private drawSprite(position: Vec2, size: Vec2, texture: WebGLTexture) {
         this.gl.vertexAttribPointer(this.texCoordAttributeLocation, 2, this.gl.FLOAT, false, 0, 0);
   }
     this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
-  }
+}
   
-  public drawScene(playerPosition: Vec2, playerSize: Vec2, platforms: Platform[], playerTexture: WebGLTexture | null, platformTexture: WebGLTexture | null) {
+public drawScene(playerPosition: Vec2, playerSize: Vec2, platforms: Platform[], playerTexture: WebGLTexture | null, platformTexture: WebGLTexture | null) {
     this.clear();
     this.gl.useProgram(this.program);
     this.gl.enableVertexAttribArray(this.positionAttributeLocation);
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.unitSquarePositionBuffer);
     this.gl.vertexAttribPointer(this.positionAttributeLocation, 2, this.gl.FLOAT, false, 0, 0);
-    this.gl.enable(this.gl.BLEND);
-    this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
+ //   this.gl.enable(this.gl.BLEND);
+  //  this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
 
     for (const platform of platforms) {
       this.drawRect(platform.position, platform.size, [0.5, 0.5, 0.5, 1.0]);
