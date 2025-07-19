06b7e08c29cbffb2873bbcdbf689f9f137e344c4
Noah Cohn <noahc42@gmail.com>
2025-07-19 07:31:41 -0700

    Update renderer.ts

--- src/gl/renderer.ts
+++ src/gl/renderer.ts
@@ -84,8 +84,14 @@ export class Renderer {
       image.onload = () => {
         this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
         this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, image);
-        this.gl.generateMipmap(this.gl.TEXTURE_2D);
-        
+      //  this.gl.generateMipmap(this.gl.TEXTURE_2D);
+                // FIX: Set texture parameters for pixel-perfect spritesheet rendering.
+        // This prevents bleeding and distortion.
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
+
         this.textureDimensions.set(texture, { width: image.width, height: image.height });
         resolve(texture);
       };
