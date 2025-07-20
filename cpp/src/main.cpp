fd61717e70ac811a75dc45ecfe8d3fe11dd99397
Noah Cohn <noahc42@gmail.com>
2025-07-20 04:59:48 -0700

    Update main.cpp

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -2,28 +2,12 @@
 #include <emscripten/bind.h>
 
 EMSCRIPTEN_BINDINGS(WASM_Venture) {
-    emscripten::value_object<Vec2>("Vec2")
-        .field("x", &Vec2::x)
-        .field("y", &Vec2::y);
-        
-    emscripten::value_object<Platform>("Platform")
-        .field("position", &Platform::position)
-        .field("size", &Platform::size);
-
+    emscripten::value_object<Vec2>("Vec2").field("x", &Vec2::x).field("y", &Vec2::y);
+    emscripten::value_object<Platform>("Platform").field("position", &Platform::position).field("size", &Platform::size);
     emscripten::register_vector<Platform>("PlatformList");
+    emscripten::value_object<InputState>("InputState").field("left", &InputState::left).field("right", &InputState::right).field("jump", &InputState::jump);
+    emscripten::value_object<AnimationState>("AnimationState").field("currentState", &AnimationState::currentState).field("currentFrame", &AnimationState::currentFrame).field("facingLeft", &AnimationState::facingLeft);
 
-    emscripten::value_object<InputState>("InputState")
-        .field("left", &InputState::left)
-        .field("right", &InputState::right)
-        .field("jump", &InputState::jump);
-
-    // Bind the AnimationState struct.
-    emscripten::value_object<AnimationState>("AnimationState")
-        .field("currentState", &AnimationState::currentState)
-        .field("currentFrame", &AnimationState::currentFrame)
-        .field("facingLeft", &AnimationState::facingLeft);
-
-    // Bind the full Game class.
     emscripten::class_<Game>("Game")
         .constructor<>()
         .function("update", &Game::update)
@@ -32,7 +16,5 @@ EMSCRIPTEN_BINDINGS(WASM_Venture) {
         .function("getCameraPosition", &Game::getCameraPosition)
         .function("getPlatforms", &Game::getPlatforms)
         .function("getPlayerAnimationState", &Game::getPlayerAnimationState)
-        // NEW: Expose the getPlayerSize function to JavaScript.
+        // FIX: This binding will now work correctly.
         .function("getPlayerSize", &Game::getPlayerSize);
-}
-
