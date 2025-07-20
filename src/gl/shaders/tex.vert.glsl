4605fe42dfa157ba1614f62e8bfc5e44dc76ce8e
Noah Cohn <noahc42@gmail.com>
2025-07-20 00:50:03 -0700

    Update tex.vert.glsl

--- src/gl/shaders/tex.vert.glsl
+++ src/gl/shaders/tex.vert.glsl
@@ -8,7 +8,6 @@ uniform vec2 u_camera_position;
 uniform vec2 u_sprite_frame_size;
 uniform vec2 u_sprite_sheet_size;
 uniform vec2 u_sprite_frame_coord;
-uniform mat4 u_projection;
 
 // A boolean to control horizontal flipping
 uniform bool u_flip_horizontal;
@@ -19,9 +18,8 @@ void main() {
   // Geometry is no longer flipped here. It remains centered.
   vec2 world_position = (a_position * u_model_size) + u_model_position;
   vec2 view_position = world_position - u_camera_position;
+  gl_Position = vec4(view_position, 0.0, 1.0);
 
-  gl_Position = u_projection * vec4(view_position, 0.0, 1.0); // Apply projection
- 
   // FIX: Flip the texture coordinate instead of the geometry.
   // This is a more stable method that avoids positional bugs.
   vec2 final_texCoord = a_texCoord;
