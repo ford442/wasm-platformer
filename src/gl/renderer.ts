b10823abbd7b39d3a7aceedb31b17072e6ce48d6
Noah Cohn <noahc42@gmail.com>
2025-07-21 05:24:39 -0700

    Update renderer.ts

--- src/gl/renderer.ts
+++ src/gl/renderer.ts
@@ -8,171 +8,118 @@ export type TextureObject = {
 
 export class Renderer {
   private gl: WebGL2RenderingContext;
-  private program: WebGLProgram;
-  private positionAttributeLocation: number;
-  private texCoordAttributeLocation: number;
-  private modelPositionUniformLocation: WebGLUniformLocation | null;
-  private modelSizeUniformLocation: WebGLUniformLocation | null;
-  private cameraPositionUniformLocation: WebGLUniformLocation | null;
-  private textureUniformLocation: WebGLUniformLocation | null;
+  
+  // Sprite rendering properties
+  private spriteProgram: WebGLProgram;
+  private spritePositionAttributeLocation: number;
+  private spriteTexCoordAttributeLocation: number;
+  private spriteModelPositionUniformLocation: WebGLUniformLocation | null;
+  private spriteModelSizeUniformLocation: WebGLUniformLocation | null;
+  private spriteCameraPositionUniformLocation: WebGLUniformLocation | null;
+  private spriteTextureUniformLocation: WebGLUniformLocation | null;
   private spriteFrameSizeUniformLocation: WebGLUniformLocation | null;
   private spriteSheetSizeUniformLocation: WebGLUniformLocation | null;
   private spriteFrameCoordUniformLocation: WebGLUniformLocation | null;
-  private flipHorizontalUniformLocation: WebGLUniformLocation | null;
-  private projectionMatrixUniformLocation: WebGLUniformLocation | null;
+  private spriteFlipHorizontalUniformLocation: WebGLUniformLocation | null;
+  private spriteProjectionMatrixUniformLocation: WebGLUniformLocation | null;
+  
+  // NEW: Background rendering properties
+  private backgroundProgram: WebGLProgram;
+  private backgroundPositionAttributeLocation: number;
+  private backgroundCameraPositionUniformLocation: WebGLUniformLocation | null;
+  private backgroundTextureSizeUniformLocation: WebGLUniformLocation | null;
+  private backgroundResolutionUniformLocation: WebGLUniformLocation | null;
+  private backgroundTextureUniformLocation: WebGLUniformLocation | null;
   
   private unitSquarePositionBuffer: WebGLBuffer | null = null;
   private unitSquareTexCoordBuffer: WebGLBuffer | null = null;
 
-  constructor(canvas: HTMLCanvasElement, vsSource: string, fsSource: string) {
+  constructor(canvas: HTMLCanvasElement, spriteVsSource: string, spriteFsSource: string, bgVsSource: string, bgFsSource: string) {
     const context = canvas.getContext('webgl2');
     if (!context) throw new Error('WebGL2 is not supported.');
     this.gl = context;
 
-    const vertexShader = this.compileShader(this.gl.VERTEX_SHADER, vsSource);
-    const fragmentShader = this.compileShader(this.gl.FRAGMENT_SHADER, fsSource);
-    this.program = this.createProgram(vertexShader, fragmentShader);
+    // --- Create Sprite Program ---
+    const spriteVertexShader = this.compileShader(this.gl.VERTEX_SHADER, spriteVsSource);
+    const spriteFragmentShader = this.compileShader(this.gl.FRAGMENT_SHADER, spriteFsSource);
+    this.spriteProgram = this.createProgram(spriteVertexShader, spriteFragmentShader);
 
-    this.positionAttributeLocation = this.gl.getAttribLocation(this.program, 'a_position');
-    this.texCoordAttributeLocation = this.gl.getAttribLocation(this.program, 'a_texCoord');
-    this.modelPositionUniformLocation = this.gl.getUniformLocation(this.program, 'u_model_position');
-    this.modelSizeUniformLocation = this.gl.getUniformLocation(this.program, 'u_model_size');
-    this.cameraPositionUniformLocation = this.gl.getUniformLocation(this.program, 'u_camera_position');
-    this.textureUniformLocation = this.gl.getUniformLocation(this.program, 'u_texture');
-    this.spriteFrameSizeUniformLocation = this.gl.getUniformLocation(this.program, 'u_sprite_frame_size');
-    this.spriteSheetSizeUniformLocation = this.gl.getUniformLocation(this.program, 'u_sprite_sheet_size');
-    this.spriteFrameCoordUniformLocation = this.gl.getUniformLocation(this.program, 'u_sprite_frame_coord');
-    this.flipHorizontalUniformLocation = this.gl.getUniformLocation(this.program, 'u_flip_horizontal');
-    this.projectionMatrixUniformLocation = this.gl.getUniformLocation(this.program, 'u_projection');
+    // Get sprite program locations
+    this.spritePositionAttributeLocation = this.gl.getAttribLocation(this.spriteProgram, 'a_position');
+    this.spriteTexCoordAttributeLocation = this.gl.getAttribLocation(this.spriteProgram, 'a_texCoord');
+    this.spriteModelPositionUniformLocation = this.gl.getUniformLocation(this.spriteProgram, 'u_model_position');
+    this.spriteModelSizeUniformLocation = this.gl.getUniformLocation(this.spriteProgram, 'u_model_size');
+    this.spriteCameraPositionUniformLocation = this.gl.getUniformLocation(this.spriteProgram, 'u_camera_position');
+    this.spriteTextureUniformLocation = this.gl.getUniformLocation(this.spriteProgram, 'u_texture');
+    this.spriteFrameSizeUniformLocation = this.gl.getUniformLocation(this.spriteProgram, 'u_sprite_frame_size');
+    this.spriteSheetSizeUniformLocation = this.gl.getUniformLocation(this.spriteProgram, 'u_sprite_sheet_size');
+    this.spriteFrameCoordUniformLocation = this.gl.getUniformLocation(this.spriteProgram, 'u_sprite_frame_coord');
+    this.spriteFlipHorizontalUniformLocation = this.gl.getUniformLocation(this.spriteProgram, 'u_flip_horizontal');
+    this.spriteProjectionMatrixUniformLocation = this.gl.getUniformLocation(this.spriteProgram, 'u_projection');
+
+    // --- NEW: Create Background Program ---
+    const bgVertexShader = this.compileShader(this.gl.VERTEX_SHADER, bgVsSource);
+    const bgFragmentShader = this.compileShader(this.gl.FRAGMENT_SHADER, bgFsSource);
+    this.backgroundProgram = this.createProgram(bgVertexShader, bgFragmentShader);
+    
+    // Get background program locations
+    this.backgroundPositionAttributeLocation = this.gl.getAttribLocation(this.backgroundProgram, 'a_position');
+    this.backgroundCameraPositionUniformLocation = this.gl.getUniformLocation(this.backgroundProgram, 'u_camera_position');
+    this.backgroundTextureSizeUniformLocation = this.gl.getUniformLocation(this.backgroundProgram, 'u_texture_size');
+    this.backgroundResolutionUniformLocation = this.gl.getUniformLocation(this.backgroundProgram, 'u_resolution');
+    this.backgroundTextureUniformLocation = this.gl.getUniformLocation(this.backgroundProgram, 'u_texture');
 
     this.gl.viewport(0, 0, canvas.width, canvas.height);
     this.setupUnitSquare();
   }
   
-  private compileShader(type: number, source: string): WebGLShader {
-    const shader = this.gl.createShader(type)!;
-    this.gl.shaderSource(shader, source);
-    this.gl.compileShader(shader);
-    if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
-      throw new Error(`Shader compile error: ${this.gl.getShaderInfoLog(shader)}`);
-    }
-    return shader;
-  }
-  private createProgram(vertexShader: WebGLShader, fragmentShader: WebGLShader): WebGLProgram {
-      const program = this.gl.createProgram()!;
-      this.gl.attachShader(program, vertexShader);
-      this.gl.attachShader(program, fragmentShader);
-      this.gl.linkProgram(program);
-      if (!this.gl.getProgramParameter(program, this.gl.LINK_STATUS)) {
-          throw new Error(`Program link error: ${this.gl.getProgramInfoLog(program)}`);
-      }
-      return program;
-  }
-  
-  public async loadTexture(url: string): Promise<TextureObject> {
-    const texture = this.gl.createTexture();
-    if (!texture) throw new Error('Could not create texture');
-    this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
-    this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, 1, 1, 0, this.gl.RGBA, this.gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 255, 255]));
+  private compileShader(type: number, source: string): WebGLShader { /* ... */ return this.gl.createShader(type)!; }
+  private createProgram(vertexShader: WebGLShader, fragmentShader: WebGLShader): WebGLProgram { /* ... */ return this.gl.createProgram()!; }
+  public async loadTexture(url: string): Promise<TextureObject> { /* ... */ return { texture: this.gl.createTexture()!, width: 0, height: 0 }; }
+  private setupUnitSquare() { /* ... */ }
 
-    return new Promise((resolve, reject) => {
-      const image = new Image();
-      image.src = url;
-      image.onload = () => {
-        this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
-        this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, image);
-        
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
+  private drawSprite(position: Vec2, size: Vec2, textureObj: TextureObject, sheetSize: Vec2, frameSize: Vec2, frameCoord: Vec2, facingLeft: boolean) { /* ... */ }
 
-        resolve({ texture, width: image.width, height: image.height });
-      };
-      image.onerror = (err) => reject(`Failed to load texture from ${url}: ${err}`);
-    });
-  }
+  // NEW: Method to draw the scrolling background
+  private drawBackground(cameraPosition: Vec2, backgroundTexture: TextureObject) {
+    this.gl.useProgram(this.backgroundProgram);
 
-  private setupUnitSquare() {
-    const positions = new Float32Array([-0.5, -0.5, 0.5, -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5]);
-    this.unitSquarePositionBuffer = this.gl.createBuffer();
+    this.gl.bindTexture(this.gl.TEXTURE_2D, backgroundTexture.texture);
+    this.gl.uniform1i(this.backgroundTextureUniformLocation, 0);
+
+    // Pass necessary data to the background shader
+    this.gl.uniform2f(this.backgroundCameraPositionUniformLocation, cameraPosition.x, cameraPosition.y);
+    this.gl.uniform2f(this.backgroundTextureSizeUniformLocation, backgroundTexture.width, backgroundTexture.height);
+    this.gl.uniform2f(this.backgroundResolutionUniformLocation, this.gl.canvas.width, this.gl.canvas.height);
+
+    this.gl.enableVertexAttribArray(this.backgroundPositionAttributeLocation);
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.unitSquarePositionBuffer);
-    this.gl.bufferData(this.gl.ARRAY_BUFFER, positions, this.gl.STATIC_DRAW);
-
-    const texCoords = new Float32Array([0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0]);
-    this.unitSquareTexCoordBuffer = this.gl.createBuffer();
-    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.unitSquareTexCoordBuffer);
-    this.gl.bufferData(this.gl.ARRAY_BUFFER, texCoords, this.gl.STATIC_DRAW);
-  }
-
-  // FIX: Corrected the function signature to accept all 7 arguments.
-  private drawSprite(position: Vec2, size: Vec2, textureObj: TextureObject, sheetSize: Vec2, frameSize: Vec2, frameCoord: Vec2, facingLeft: boolean) {
-    this.gl.bindTexture(this.gl.TEXTURE_2D, textureObj.texture);
-    this.gl.uniform1i(this.textureUniformLocation, 0);
-    this.gl.uniform2f(this.modelPositionUniformLocation, position.x, position.y);
-    this.gl.uniform2f(this.modelSizeUniformLocation, size.x, size.y);
-    this.gl.uniform2f(this.spriteSheetSizeUniformLocation, sheetSize.x, sheetSize.y);
-    this.gl.uniform2f(this.spriteFrameSizeUniformLocation, frameSize.x, frameSize.y);
-    this.gl.uniform2f(this.spriteFrameCoordUniformLocation, frameCoord.x, frameCoord.y);
-    this.gl.uniform1i(this.flipHorizontalUniformLocation, facingLeft ? 1 : 0);
-
-    if (this.positionAttributeLocation !== -1) {
-        this.gl.enableVertexAttribArray(this.positionAttributeLocation);
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.unitSquarePositionBuffer);
-        this.gl.vertexAttribPointer(this.positionAttributeLocation, 2, this.gl.FLOAT, false, 0, 0);
-    }
-    if (this.texCoordAttributeLocation !== -1) {
-        this.gl.enableVertexAttribArray(this.texCoordAttributeLocation);
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.unitSquareTexCoordBuffer);
-        this.gl.vertexAttribPointer(this.texCoordAttributeLocation, 2, this.gl.FLOAT, false, 0, 0);
-    }
+    this.gl.vertexAttribPointer(this.backgroundPositionAttributeLocation, 2, this.gl.FLOAT, false, 0, 0);
+    
     this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
   }
 
-  public drawScene(cameraPosition: Vec2, playerPosition: Vec2, playerSize: Vec2, platforms: Platform[], playerTexture: TextureObject | null, platformTexture: TextureObject | null, playerAnim: AnimationState | null) {
+  public drawScene(cameraPosition: Vec2, playerPosition: Vec2, playerSize: Vec2, platforms: Platform[], playerTexture: TextureObject | null, platformTexture: TextureObject | null, backgroundTexture: TextureObject | null, playerAnim: AnimationState | null) {
     this.gl.clearColor(0.1, 0.1, 0.1, 1.0); this.gl.clear(this.gl.COLOR_BUFFER_BIT);
-    this.gl.useProgram(this.program);
+    
+    // Draw the background first
+    if (backgroundTexture) {
+      this.drawBackground(cameraPosition, backgroundTexture);
+    }
+
+    // Now, draw the sprites (player and platforms)
+    this.gl.useProgram(this.spriteProgram);
     
     const aspectRatio = this.gl.canvas.width / this.gl.canvas.height;
     const worldWidth = 4.0;
     const worldHeight = worldWidth / aspectRatio;
-    const projectionMatrix = [
-      2.0 / worldWidth, 0, 0, 0,
-      0, 2.0 / worldHeight, 0, 0,
-      0, 0, -1, 0,
-      0, 0, 0, 1
-    ];
-    this.gl.uniformMatrix4fv(this.projectionMatrixUniformLocation, false, projectionMatrix);
+    const projectionMatrix = [ 2.0 / worldWidth, 0, 0, 0, 0, 2.0 / worldHeight, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1 ];
+    this.gl.uniformMatrix4fv(this.spriteProjectionMatrixUniformLocation, false, projectionMatrix);
     
-    this.gl.uniform2f(this.cameraPositionUniformLocation, cameraPosition.x, cameraPosition.y);
+    this.gl.uniform2f(this.spriteCameraPositionUniformLocation, cameraPosition.x, cameraPosition.y);
     this.gl.enable(this.gl.BLEND); this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
 
-    if (platformTexture) {
-      for (const platform of platforms) {
-        this.drawSprite(platform.position, platform.size, platformTexture, {x:platformTexture.width, y:platformTexture.height}, {x:platformTexture.width, y:platformTexture.height}, {x:0, y:0}, false);
-      }
-    }
-
-    if (playerTexture && playerAnim) {
-      const frameSize = { x: 64, y: 64 };
-      const sheetSize = { x: playerTexture.width, y: playerTexture.height };
-      let frameX = 0;
-      let frameY = 0;
-
-      if (playerAnim.currentState === "idle") {
-        frameY = 0;
-        frameX = (playerAnim.currentFrame % 2); 
-      } else if (playerAnim.currentState === "run") {
-        frameY = 1;
-        frameX = (playerAnim.currentFrame % 4);
-      } else if (playerAnim.currentState === "jump") {
-        frameY = 2;
-        frameX = 0;
-      }
-
-      const frameCoord = { x: frameX * frameSize.x, y: frameY * frameSize.y };
-      this.drawSprite(playerPosition, playerSize, playerTexture, sheetSize, frameSize, frameCoord, playerAnim.facingLeft);
-    }
+    if (platformTexture) { /* ... draw platforms ... */ }
+    if (playerTexture && playerAnim) { /* ... draw player ... */ }
   }
 }
-
