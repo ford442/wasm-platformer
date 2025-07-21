13f2905dafd1199df5cab8528ac2559c99d9608b
Noah Cohn <noahc42@gmail.com>
2025-07-21 06:01:47 -0700

    Update background.frag.glsl

--- src/gl/shaders/background.frag.glsl
+++ src/gl/shaders/background.frag.glsl
@@ -5,13 +5,8 @@ uniform sampler2D u_texture;
 uniform vec2 u_camera_position;
 
 void main() {
-  // Define how fast the background scrolls relative to the camera.
   float parallaxFactor = 0.1;
-  // Scale the camera's world position to create a noticeable UV offset.
   float scrollOffset = u_camera_position.x * parallaxFactor * 0.1;
-
-  // Apply the offset to the texture coordinate.
   vec2 final_uv = vec2(v_texCoord.x + scrollOffset, v_texCoord.y);
-  
   outColor = texture(u_texture, final_uv);
 }
