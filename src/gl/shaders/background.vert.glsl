d77657f7f4d973f7d78461c8efc48872fb951dc1
Noah Cohn <noahc42@gmail.com>
2025-07-21 06:01:36 -0700

    Update background.vert.glsl

--- src/gl/shaders/background.vert.glsl
+++ src/gl/shaders/background.vert.glsl
@@ -2,8 +2,6 @@
 in vec2 a_position;
 out vec2 v_texCoord;
 void main() {
-  // Pass the vertex position directly to the fragment shader.
-  // This covers the entire screen since we use a -1 to 1 quad.
   v_texCoord = a_position * 0.5 + 0.5;
   gl_Position = vec4(a_position, 0.0, 1.0);
 }
