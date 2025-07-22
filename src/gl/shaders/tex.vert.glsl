f5e9564863ee7df9a0ffb4b78470c6115e72ac12
Noah Cohn <noahc42@gmail.com>
2025-07-21 23:19:47 -0700

    Update tex.vert.glsl

--- src/gl/shaders/tex.vert.glsl
+++ src/gl/shaders/tex.vert.glsl
@@ -1,25 +1,7 @@
 #version 300 es
 in vec2 a_position;
-in vec2 a_texCoord;
-uniform vec2 u_model_position;
-uniform vec2 u_model_size;
-uniform vec2 u_camera_position;
-uniform vec2 u_sprite_frame_size;
-uniform vec2 u_sprite_sheet_size;
-uniform vec2 u_sprite_frame_coord;
-uniform bool u_flip_horizontal;
-uniform mat4 u_projection;
 out vec2 v_texCoord;
-
 void main() {
-  vec2 world_position = (a_position * u_model_size) + u_model_position;
-  vec2 view_position = world_position - u_camera_position;
-  gl_Position = u_projection * vec4(view_position, 0.0, 1.0);
-  
-  vec2 final_texCoord = a_texCoord;
-  if (u_flip_horizontal) {
-    final_texCoord.x = 1.0 - final_texCoord.x;
-  }
-  vec2 texelSize = u_sprite_frame_size / u_sprite_sheet_size;
-  v_texCoord = (u_sprite_frame_coord / u_sprite_sheet_size) + (final_texCoord * texelSize);
+  v_texCoord = a_position * 0.5 + 0.5;
+  gl_Position = vec4(a_position, 0.0, 1.0);
 }
