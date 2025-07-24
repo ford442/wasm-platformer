12a2a021194b723f52ccaf91198dd8d368689d06
Noah Cohn <noahc42@gmail.com>
2025-07-24 10:01:50 -0700

    Update tex.vert.glsl

--- src/gl/shaders/tex.vert.glsl
+++ src/gl/shaders/tex.vert.glsl
@@ -1,14 +1,24 @@
 #version 300 es
+
 in vec2 a_position;
 in vec2 a_texCoord;
+
+uniform mat4 u_projection;
+uniform vec2 u_camera_position;
 uniform vec2 u_model_position;
 uniform vec2 u_model_size;
-uniform vec2 u_camera_position;
+
 out vec2 v_texCoord;
 
 void main() {
+  // Calculate the world position of the vertex
   vec2 world_position = (a_position * u_model_size) + u_model_position;
+  
+  // Calculate the position relative to the camera
   vec2 view_position = world_position - u_camera_position;
-  gl_Position = vec4(view_position, 0.0, 1.0);
+
+  // Apply the projection matrix
+  gl_Position = u_projection * vec4(view_position, 0.0, 1.0);
+  
   v_texCoord = a_texCoord;
 }
