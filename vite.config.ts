1f7933759148661d6ec3b1655ad8590f5fc920b7
Noah Cohn <noahc42@gmail.com>
2025-07-25 06:49:04 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,34 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Add this line:
-  base: './', 
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Add the viteStaticCopy plugin
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/filament/dist/filament-wasm.*',
+          dest: '.'
+        },
+        {
+          src: 'node_modules/filament/dist/ibl.ktx',
+          dest: '.'
+        }
+      ]
+    })
+  ],
+  // Required to enable SharedArrayBuffer, which Filament uses.
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
+  },
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
+  },
 })
