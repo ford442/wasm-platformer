7e11e2d38e3e29d5184b77d19d1a2f60edb057f3
Noah Cohn <noahc42@gmail.com>
2025-07-26 02:00:13 -0700

    Update background.vert.glsl

--- src/gl/shaders/background.vert.glsl
+++ src/gl/shaders/background.vert.glsl
@@ -2,7 +2,6 @@
 in vec2 a_position;
 out vec2 v_texCoord;
 void main() {
-  // Pass the vertex position directly. This covers the entire screen.
   v_texCoord = a_position * 0.5 + 0.5;
   gl_Position = vec4(a_position, 0.0, 1.0);
 }
