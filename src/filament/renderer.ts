07ddd3e7f8878e952db6cee02967a8d8179219f0
Noah Cohn <noahc42@gmail.com>
2025-07-25 20:51:13 -0700

    Update renderer.ts

--- src/filament/renderer.ts
+++ src/filament/renderer.ts
@@ -50,7 +50,7 @@ export class FilamentRenderer {
 
         const light = Filament.EntityManager.get().create();
         
-        new Filament.LightManager$Builder(Filament.LightManager$Type.SUN)
+        new Filament.LightManager$Builder()
             .color([0.7, 0.7, 0.7])
             .intensity(50000.0)
             .direction([0, -1, 0])
