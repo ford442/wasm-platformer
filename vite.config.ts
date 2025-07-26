41a0e1f11335ca995bdb35f278c5697f054fc250
Noah Cohn <noahc42@gmail.com>
2025-07-25 20:37:02 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     viteStaticCopy({
       targets: [
         {
-          src: 'node_modules/filament/filament-wasm.*',
+          src: 'node_modules/filament/filament.*',
           dest: '.'
         }
       ]
