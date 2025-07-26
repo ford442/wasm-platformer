abb8f87b8b0fbe8e229824ffecd18202d611a3d1
Noah Cohn <noahc42@gmail.com>
2025-07-26 01:58:55 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// Import the main App component from its location in the components folder
 import App from './components/App'; 
-// Import the global CSS styles
 import './index.css';
 
-// This is the standard React 18 entry point.
-// It finds the <div id="root"> in index.html and renders the App component into it.
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
